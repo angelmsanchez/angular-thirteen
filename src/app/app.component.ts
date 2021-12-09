fa1f2b5abee31166dd75a4da7f1b3a2229d0213a
angel <angel.munoz@tokiota.com>
2021-12-09 09:00:58 +0100

    add linter to project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'angular-thirteen';
+
+
+export class AppComponent implements OnInit {
+
+  title  = 'angular-thirteen';
+
+  ngOnInit(): void{
+    console.log('linter');
+  }
 }
